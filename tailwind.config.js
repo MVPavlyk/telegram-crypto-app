23464299e863316930c2df3885fec55eec86d988
mykola.pavlyk <mykola.pavlyk@otakoyi.com>
2024-07-17 08:03:29 +0300

    fix: config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
   theme: {
     screens: {
       xs: '380px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
     },
     extend: {
       colors: {
