ba60e2c7abeacc95815756dca13ecdf20d1c1194
mykola.pavlyk <mykola.pavlyk@otakoyi.com>
2024-07-18 09:12:45 +0300

    feat: wallet initial

---
+++ src/assets/icons/WalletOpenIcon.tsx
@@ -0,0 +1,15 @@
+const WalletOpenIcon = () => {
+  return (
+    <svg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M10.6668 7.33333L13.3335 4.66667M13.3335 4.66667L10.6668 2M13.3335 4.66667L2.66683 4.66667M5.3335 8.66667L2.66683 11.3333M2.66683 11.3333L5.3335 14M2.66683 11.3333L13.3335 11.3333'
+        stroke='white'
+        strokeWidth='1.8'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
+
+export default WalletOpenIcon;
