5fef7185c910de082177f126008790b3f35b3b78
mykola.pavlyk <mykola.pavlyk@otakoyi.com>
2024-08-04 21:24:41 +0300

    feat: game process hello vs label

--- src/modules/game/components/versus-screen.component.tsx
+++ src/modules/game/components/versus-screen.component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { TGameState } from '../types/game.types.ts';
 import classNames from 'classnames';
 import vsFrame from '../../../assets/vs-frame.png';
+import { useAppStore } from '../../common/store';
 
 export type VersusScreenProps = {
   gameState: TGameState;
@@ -9,6 +10,8 @@ export type VersusScreenProps = {
 };
 
 const VersusScreen: React.FC<VersusScreenProps> = ({ gameState, countDown }) => {
+  const { user } = useAppStore((state) => ({ ...state, user: state.user!, statistics: state.statistics! }));
+
   return (
     <section
       className={classNames(
@@ -17,13 +20,17 @@ const VersusScreen: React.FC<VersusScreenProps> = ({ gameState, countDown }) => 
       )}
     >
       <div className='absolute w-[130%] -left-[12%] h-screen z-[12] flex items-center'>
-        <img src={vsFrame} alt='vs' />
+        <img className='w-full' src={vsFrame} alt='vs' />
       </div>
       <div className='w-screen h-screen flex items-center justify-center z-[13] text-white absolute text-[60px] font-bold'>
         {countDown ? countDown : 'VS'}
       </div>
-      <div className='w-screen h-1/2 absolute left-0 top-0 game-opponent-avatar' />
-      <div className='w-screen h-1/2 absolute left-0 bottom-0 game-my-avatar' />
+      <div className='w-screen h-1/2 absolute left-0 top-0 game-opponent-avatar flex justify-center items-start py-8'>
+        <p className='text-[60px] font-bold game-preparing-username-color'>bot</p>
+      </div>
+      <div className='w-screen h-1/2 absolute left-0 bottom-0 game-my-avatar flex justify-center items-end py-8'>
+        <p className='text-[60px] font-bold game-preparing-username-color'>{user?.telegramUsername}</p>
+      </div>
     </section>
   );
 };
